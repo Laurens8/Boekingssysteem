f3608906f04fa026bd6040387fdcd7fa36afc102
Laurens8 <r0901293@student.thomasmore.be>
2023-05-03 03:37:23 +0200

    pagina refresh + status onbekend 23u

--- Boekingssysteem/Controllers/TVController.cs
+++ Boekingssysteem/Controllers/TVController.cs
@@ -2,7 +2,10 @@
 using Boekingssysteem.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Boekingssysteem.Controllers
 {
@@ -13,22 +16,45 @@ namespace Boekingssysteem.Controllers
         public TVController(BoekingssysteemContext context)
         {
             _context = context;
-        }
+        }     
 
-        public IActionResult Index()
+        public async Task <IActionResult> Index()
         {
-            var personen = _context.Personen.ToList();
+            var personen = _context.Personen.ToList();           
             var persoonrichtingen = _context.PersoonRichtingen.Include(x => x.Persoon).Include(z => z.Richting);
             var persoonfuncties = _context.PersoonFuncties.Include(x => x.Persoon).Include(z => z.Functie);
             var richtingen = _context.Richtingen;
             var afwezigheden = _context.Afwezigheden;
+
+            if (DateTime.Now.Hour == 23)
+            {
+                foreach (var item in personen)
+                {
+                    item.Aanwezig = null;
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+
+                    foreach (var item2 in afwezigheden)
+                    {
+                        if (item2.Begindatum <= item2.Einddatum)
+                        {
+                            item.Aanwezig = false;
+                            _context.Update(item);
+                            await _context.SaveChangesAsync();
+                        }
+                    }
+                }                
+            }
+
             PersoonListViewModel plvm = new PersoonListViewModel();
             plvm.Personen = personen;
             plvm.PersoonRichtingen = persoonrichtingen.ToList();
             plvm.PersoonFuncties = persoonfuncties.ToList();
             plvm.Richtingen = richtingen.ToList();
             plvm.Afwezigheden = afwezigheden.ToList();
+          
             return View(plvm);
         }
+
     }
 }
